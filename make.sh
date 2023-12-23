c58c3d38df31fbb6d247ea17834e7e7851e69d07
shok1122 <shok1122@zohomail.com>
2023-12-23 14:59:58 +0000

    Added

---
+++ make.sh
@@ -0,0 +1,29 @@
+REPO=shok1122
+
+PATH_DIR="$1"
+COMMAND="$2"
+
+list=(${PATH_DIR//\// })
+img=${list[0]}
+ver=${list[1]}
+
+echo "image: $img"
+echo "version: $ver"
+echo "command: $COMMAND"
+
+(
+	cd $PATH_DIR
+
+	if [ $COMMAND = "build" ]; then
+		docker build -t ${REPO}/${img}:${ver} .
+	fi
+
+	if [ $COMMAND = "push" ]; then
+		docker push ${REPO}/${img}:${ver}
+	fi
+
+	if [ $COMMAND = "run" ]; then
+		docker run -it --rm ${REPO}/${img}:${ver} bash
+	fi
+)
+
